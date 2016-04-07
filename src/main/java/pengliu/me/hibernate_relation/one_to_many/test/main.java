e52d8ea3f5942772ece4464f542da8164d099ef2
liupeng330 <330liupeng@gmail.com>
2016-04-07 15:00:09 +0800

    init

--- src/main/java/pengliu/me/hibernate_relation/one_to_many/test/main.java
+++ src/main/java/pengliu/me/hibernate_relation/one_to_many/test/main.java
@@ -1,33 +1,63 @@
 package pengliu.me.hibernate_relation.one_to_many.test;
 
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import pengliu.me.hibernate_relation.HibernateUtil;
 import pengliu.me.hibernate_relation.one_to_many.domain.StockDailyRecordEntity;
 import pengliu.me.hibernate_relation.one_to_many.domain.StockEntity;
 
-import java.sql.Timestamp;
 import java.util.Date;
+import java.sql.Timestamp;
+import java.util.List;
 
 public class main
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws Exception
     {
         Session session = HibernateUtil.getSessionFactory().openSession();
-        session.beginTransaction();
-        StockEntity stockEntity = new StockEntity();
-        stockEntity.setStockCode("123");
-        stockEntity.setStockName("liupeng");
-        session.save(stockEntity);
+        Transaction tx = null;
+        try
+        {
+            tx = session.beginTransaction();
 
-        StockDailyRecordEntity stockDailyRecordEntity = new StockDailyRecordEntity();
-        stockDailyRecordEntity.setDate(new Timestamp(new Date().getTime()));
-        stockDailyRecordEntity.setPriceChange("34.3");
-        stockDailyRecordEntity.setPriceOpen("ahah");
-        stockDailyRecordEntity.setPriceClose("close");
-        stockDailyRecordEntity.setStock(stockEntity);
-        stockDailyRecordEntity.setVolume("volume");
-        session.save(stockDailyRecordEntity);
+//            StockEntity stockEntity = new StockEntity();
+//            stockEntity.setStockCode("123");
+//            stockEntity.setStockName("liupeng");
+//            session.save(stockEntity);
+//
+//            StockDailyRecordEntity stockDailyRecordEntity = new StockDailyRecordEntity();
+//            stockDailyRecordEntity.setDate(new Timestamp(new Date().getTime()));
+//            stockDailyRecordEntity.setPriceChange("34.3");
+//            stockDailyRecordEntity.setPriceOpen("ahah");
+//            stockDailyRecordEntity.setPriceClose("close");
+//            stockDailyRecordEntity.setStock(stockEntity);
+//            stockDailyRecordEntity.setVolume("volume");
+//            session.save(stockDailyRecordEntity);
 
-        session.getTransaction().commit();
+            //一对多关系，从一的一边获取多个对应实体
+            List ret = session.createQuery("from StockEntity where id = ?").setParameter(0, 6).list();
+
+            for(StockDailyRecordEntity i: ((StockEntity)ret.get(0)).getStockDailyRecordEntities())
+            {
+                System.out.println(i.getDailyRecordId());
+            }
+
+
+            //多对一关系，从多的一边获取一个对应实体
+            ret = session.createQuery("from StockDailyRecordEntity where id = ?").setParameter(0, 3).list();
+            System.out.println(((StockDailyRecordEntity)ret.get(0)).getStock().getStockName());
+
+
+            tx.commit();
+        }
+        catch (Exception e)
+        {
+            if (tx!=null) tx.rollback();
+            throw e;
+        }
+        finally
+        {
+            session.close();
+        }
     }
 }
